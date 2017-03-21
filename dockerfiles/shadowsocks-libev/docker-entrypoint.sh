ccd67bc2b80e3d71377810b875ca83f3b9f1e4f3
d0u9 <d0u9.su@outlook.com>
2017-03-22 00:45:27 +0800

    dockerfiles: update shadowsocks-libev
    Now, this image both support server side and client side.
    
    Also kcptun is added.

--- dockerfiles/shadowsocks-libev/docker-entrypoint.sh
+++ dockerfiles/shadowsocks-libev/docker-entrypoint.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
 
-CONFIG_FILE=${CONFIG_FILE:-"NULL"}
-
-if [ ! -f $CONFIG_FILE ]; then
-    echo "CONFIG_FILE = $CONFIG_FILE is not a regular file"
+cmd=${cmd:-"client"}
+if [ "$cmd" != "client" ] && [ "$cmd" != "server" ]; then
+    echo "Invalid command"
     exit 1
 fi
 
-exec gosu nobody:nogroup /usr/bin/ss-local -c $CONFIG_FILE
+config_file=${config_file:-"NULL"}
+if [ ! -f $config_file ]; then
+    echo "config_file = $config_file is not a regular file"
+    exit 1
+fi
+
+enable_kcptun=${enable_kcptun+"ENABLE"}
+kcpargs="$kcpargs"
+if [ "$cmd" = "client" ]; then
+    if [ "$enable_kcptun" = "ENABLE" ]; then
+        eval "gosu nobody:nogroup /usr/local/bin/kcp-client $kcpargs" &
+        while true; do
+            port=$(gosu nobody:nogroup netstat -tlnp | awk '($7 ~ /kcp-client/){print $4}' | awk 'BEGIN{FS=":"}; {print $NF}')
+            [ -z "$port" ] || break
+        done
+        exec gosu nobody:nogroup /usr/bin/ss-local -c $config_file -s 127.0.0.1 -p $port
+    else
+        exec gosu nobody:nogroup /usr/bin/ss-local -c $config_file
+    fi
+fi
+
+if [ "$cmd" = "server" ]; then
+    exec gosu nobody:nogroup /usr/bin/ss-server -c $config_file &
+
+    if [ "$enable_kcptun" = "ENABLE" ]; then
+        eval "gosu nobody:nogroup /usr/local/bin/kcp-server $kcpargs"
+    fi
+fi
