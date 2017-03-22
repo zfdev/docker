2bc1fdddefcde1cf178be0c795bf133ecafccf3c
d0u9 <d0u9.su@outlook.com>
2017-03-22 17:11:33 +0800

    dockerfile: shadowsocks-libev
    use KCP configuration file instead of configuration environment.

--- dockerfiles/shadowsocks-libev/docker-entrypoint.sh
+++ dockerfiles/shadowsocks-libev/docker-entrypoint.sh
@@ -6,31 +6,37 @@ if [ "$cmd" != "client" ] && [ "$cmd" != "server" ]; then
     exit 1
 fi
 
-config_file=${config_file:-"NULL"}
-if [ ! -f $config_file ]; then
-    echo "config_file = $config_file is not a regular file"
+ss_config_file=${ss_config_file:-"NULL"}
+if [ ! -f $ss_config_file ]; then
+    echo "shadowsocks-libev config file = $ss_config_file is not a regular file or does not exist"
     exit 1
 fi
 
-enable_kcptun=${enable_kcptun+"ENABLE"}
-kcpargs="$kcpargs"
+kcp_config_file=${kcp_config_file:-"NULL"}
+if [ ! -f $kcp_config_file ]; then
+    echo "kcptun config file = $kcp_config_file is not a regular file or does not exist"
+    exit 1
+fi
+
+enable_kcp=${enable_kcp+"ENABLE"}
 if [ "$cmd" = "client" ]; then
-    if [ "$enable_kcptun" = "ENABLE" ]; then
-        eval gosu nobody:nogroup /usr/local/bin/kcp-client $kcpargs > /dev/null 2>&1 &
-        while true; do
-            port=$(gosu nobody:nogroup netstat -tlnp | awk '($7 ~ /kcp-client/){print $4}' | awk 'BEGIN{FS=":"}; {print $NF}')
-            [ -z "$port" ] || break
-        done
-        exec gosu nobody:nogroup /usr/bin/ss-local -c $config_file -s 127.0.0.1 -p $port > /dev/null 2>&1
+    if [ "$enable_kcp" = "ENABLE" ]; then
+        exec gosu nobody:nogroup /usr/local/bin/kcp-client -c $kcp_config_file > /dev/null 2>&1 &
+        port=$(cat $kcp_config_file | jq '.localaddr'  | tr -d '"' | awk 'BEGIN{FS=":"}; {print $2}')
+        if [ "$port" = "null" ]; then
+            exec gosu nobody:nogroup /usr/bin/ss-local -c $ss_config_file -s 127.0.0.1 > /dev/null 2>&1
+        else
+            exec gosu nobody:nogroup /usr/bin/ss-local -c $ss_config_file -s 127.0.0.1 -p $port > /dev/null 2>&1
+        fi
     else
-        exec gosu nobody:nogroup /usr/bin/ss-local -c $config_file 2>&1 > /dev/null
+        exec gosu nobody:nogroup /usr/bin/ss-local -c $ss_config_file 2>&1 > /dev/null
     fi
 fi
 
 if [ "$cmd" = "server" ]; then
-    exec gosu nobody:nogroup /usr/bin/ss-server -c $config_file > /dev/null 2>&1 &
+    exec gosu nobody:nogroup /usr/bin/ss-server -c $ss_config_file > /dev/null 2>&1 &
 
-    if [ "$enable_kcptun" = "ENABLE" ]; then
-        eval gosu nobody:nogroup /usr/local/bin/kcp-server $kcpargs > /dev/null 2>&1
+    if [ "$enable_kcp" = "ENABLE" ]; then
+        exec gosu nobody:nogroup /usr/local/bin/kcp-server -c $kcp_config_file > /dev/null 2>&1
     fi
 fi
