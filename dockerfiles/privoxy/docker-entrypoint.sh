7e5419a5717a7979a00490c004454a63337d0ad1
d0u9 <d0u9.su@outlook.com>
2017-04-07 10:14:22 +0800

    dockerfiles: update privoxy

--- dockerfiles/privoxy/docker-entrypoint.sh
+++ dockerfiles/privoxy/docker-entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-config_file=${config_file:-"NULL"}
+config_file=${CONFIG_FILE:-"NULL"}
 if [ ! -f "$config_file" ]; then
     echo "Not a valid config file"
     exit 1
 fi
 
-exec gosu nobody:nogroup privoxy --no-daemon $config_file
+
+if [[ "$*" == privoxy*start* ]]; then
+    exec gosu nobody:nogroup privoxy --no-daemon $config_file
+fi
+
+exec "$@"
 
